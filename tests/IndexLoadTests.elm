6acee99c73029826744a62d6c19e96e6a1fa4f05
Robin Luiten <robin.luiten+github@gmail.com>
2021-02-22 13:21:11 +1000

    test: refactor tests
    After reading more doc on elm-explorations/test switch to some
    better patterns seen in doc.
    Make more readable.
    Improve the test helpers.
    Make more concise with better atomicity of test case data and test.
    Easier to read using Function Application operators  "|>" "<|".

--- tests/IndexLoadTests.elm
+++ tests/IndexLoadTests.elm
@@ -1,182 +1,134 @@
-module IndexLoadTests exposing (IndexAndListResult, IndexResult, MyDoc, config1, config2, createMyStopWordFilter, doc1, exampleJsonIndex100, exampleJsonIndex100default, exampleJsonIndex100somestring, exampleJsonIndex101, index0, indexfromString1, jsonIndexDefault, loadIndexWith1, loadIndexWithErr1, loadIndexWithErr2, myStopWords, tests)
+module IndexLoadTests exposing
+    ( indexfromString1Test
+    , loadIndexWith1Test
+    , loadIndexWithErr1Test
+    , loadIndexWithErr2Test
+    )
 
 import ElmTextSearch
+import Expect
 import Index
 import Index.Load
 import Index.Model exposing (Index(..))
-import Index.Utils
-import Stemmer
-import StopWordFilter
+import Json.Decode exposing (Error(..))
 import Test exposing (..)
-import TestUtils exposing (expectOk, expectResultFailureMessage, mapDecodeErrorToString)
-import TokenProcessors
+import TestUtils
 
 
-tests : Test
-tests =
-    describe "LoadIndex tests"
-        [ loadIndexWithErr1 ()
-        , loadIndexWithErr2 ()
-        , loadIndexWith1 ()
-        , indexfromString1 ()
-        ]
+loadIndexWithErr1Test : Test
+loadIndexWithErr1Test =
+    test "Fails to load an index with wrong index version" <|
+        \() ->
+            String.concat
+                [ "{\"indexVersion\":\"1.0.1\",\"indexType\":\"- IndexTest Type -\","
+                , "\"documentStore\":{\"doc1\":[\"banana\",\"exampl\",\"grown\",\"salli\",\"word\",\"write\"]},"
+                , "\"corpusTokens\":[\"banana\",\"exampl\",\"grown\",\"salli\",\"word\",\"write\"],"
+                , "\"tokenStore\":{\"b\":{\"a\":{\"n\":{\"a\":{\"n\":{\"a\":{\"doc1\":2.7}}}}}},"
+                , "\"e\":{\"x\":{\"a\":{\"m\":{\"p\":{\"l\":{\"doc1\":2.5}}}}}},"
+                , "\"g\":{\"r\":{\"o\":{\"w\":{\"n\":{\"doc1\":0.2}}}}},"
+                , "\"s\":{\"a\":{\"l\":{\"l\":{\"i\":{\"doc1\":0.2}}}}},"
+                , "\"w\":{\"o\":{\"r\":{\"d\":{\"doc1\":0.2}}},"
+                , "\"r\":{\"i\":{\"t\":{\"e\":{\"doc1\":0.2}}}}}}}"
+                ]
+                |> Index.Load.loadIndexWith
+                    [ { indexType = "_______some string"
+                      , ref = .cid
+                      , fields = [ ( .title, 5 ), ( .body, 1 ) ]
+                      , listFields = []
+                      , initialTransformFactories = []
+                      , transformFactories = []
+                      , filterFactories = []
+                      }
+                    ]
+                |> TestUtils.getErrorIgnoreResult
+                |> TestUtils.getDecodeErrorFailureMessage
+                |> Expect.equal "Error cannot load Index. Version supported is 1.1.0. Version tried to load is 1.0.1."
 
 
-{-| example record type for tests
--}
-type alias MyDoc =
-    { cid : String
-    , title : String
-    , author : String
-    , body : String
-    }
+loadIndexWithErr2Test : Test
+loadIndexWithErr2Test =
+    test "Fails to load an index with an indexType not in configuration provided." <|
+        \() ->
+            String.concat
+                [ "{\"indexVersion\":\"1.1.0\",\"indexType\":\"__IndexTest Type -\","
+                , "\"documentStore\":{\"doc1\":[\"banana\",\"exampl\",\"grown\",\"salli\",\"word\",\"write\"]},"
+                , "\"corpusTokens\":[\"banana\",\"exampl\",\"grown\",\"salli\",\"word\",\"write\"],"
+                , "\"tokenStore\":{\"b\":{\"a\":{\"n\":{\"a\":{\"n\":{\"a\":{\"doc1\":2.7}}}}}},"
+                , "\"e\":{\"x\":{\"a\":{\"m\":{\"p\":{\"l\":{\"doc1\":2.5}}}}}},"
+                , "\"g\":{\"r\":{\"o\":{\"w\":{\"n\":{\"doc1\":0.2}}}}},"
+                , "\"s\":{\"a\":{\"l\":{\"l\":{\"i\":{\"doc1\":0.2}}}}},"
+                , "\"w\":{\"o\":{\"r\":{\"d\":{\"doc1\":0.2}}},"
+                , "\"r\":{\"i\":{\"t\":{\"e\":{\"doc1\":0.2}}}}}}}"
+                ]
+                |> Index.Load.loadIndexWith
+                    [ { indexType = "_______some string not matching the encoded index type"
+                      , ref = .cid
+                      , fields = [ ( .title, 5 ), ( .body, 1 ) ]
+                      , listFields = []
+                      , initialTransformFactories = []
+                      , transformFactories = []
+                      , filterFactories = []
+                      }
+                    ]
+                |> TestUtils.getErrorIgnoreResult
+                |> TestUtils.getDecodeErrorFailureMessage
+                |> Expect.equal "Error cannot load Index. Tried to load index of type \"__IndexTest Type -\". It is not in supported index configurations."
 
 
-doc1 : () -> MyDoc
-doc1 _ =
-    { cid = "doc1"
-    , title = "Examples of a Banana"
-    , author = "Sally Apples"
-    , body = "Sally writes words about a grown banana."
-    }
-
-
-{-| example index
--}
-index0 : Index MyDoc
-index0 =
-    Index.new
-        { indexType = "- IndexTest Type -"
-        , ref = .cid
-        , fields =
-            [ ( .title, 5 )
-            , ( .body, 1 )
-            ]
-        , listFields = []
-        }
-
-
-type alias IndexResult =
-    Result String (Index MyDoc)
-
-
-type alias IndexAndListResult =
-    Result String ( Index MyDoc, List ( String, Float ) )
-
-
-myStopWords =
-    [ "electronic", "harvesting", "pawpaw" ]
-
-
-createMyStopWordFilter =
-    StopWordFilter.createFilterFuncWith myStopWords
-
-
-config1 =
-    { indexType = "_______some string"
-    , ref = .cid
-    , fields =
-        [ ( .title, 5 )
-        , ( .body, 1 )
-        ]
-    , listFields = []
-    , initialTransformFactories =
-        [ Index.Utils.createFuncCreator TokenProcessors.trimmer ]
-    , transformFactories =
-        [ Index.Utils.createFuncCreator Stemmer.stem
-        ]
-    , filterFactories =
-        [ createMyStopWordFilter
-        ]
-    }
-
-
-config2 =
-    { indexType = "@@@@@@@some string"
-    , ref = .cid
-    , fields =
-        [ ( .title, 5 )
-        , ( .body, 1 )
-        ]
-    , listFields = []
-    , initialTransformFactories =
-        [ Index.Utils.createFuncCreator TokenProcessors.trimmer ]
-    , transformFactories =
-        [ Index.Utils.createFuncCreator Stemmer.stem
-        ]
-    , filterFactories =
-        [ createMyStopWordFilter
-        ]
-    }
-
-
-{-| encoded variants for testing load
--}
-exampleJsonIndex100 =
-    "{\"indexVersion\":\"1.1.0\",\"indexType\":\"__IndexTest Type -\",\"documentStore\":{\"doc1\":[\"banana\",\"exampl\",\"grown\",\"salli\",\"word\",\"write\"]},\"corpusTokens\":[\"banana\",\"exampl\",\"grown\",\"salli\",\"word\",\"write\"],\"tokenStore\":{\"b\":{\"a\":{\"n\":{\"a\":{\"n\":{\"a\":{\"doc1\":2.7}}}}}},\"e\":{\"x\":{\"a\":{\"m\":{\"p\":{\"l\":{\"doc1\":2.5}}}}}},\"g\":{\"r\":{\"o\":{\"w\":{\"n\":{\"doc1\":0.2}}}}},\"s\":{\"a\":{\"l\":{\"l\":{\"i\":{\"doc1\":0.2}}}}},\"w\":{\"o\":{\"r\":{\"d\":{\"doc1\":0.2}}},\"r\":{\"i\":{\"t\":{\"e\":{\"doc1\":0.2}}}}}}}"
-
-
-exampleJsonIndex101 =
-    "{\"indexVersion\":\"1.0.1\",\"indexType\":\"- IndexTest Type -\",\"documentStore\":{\"doc1\":[\"banana\",\"exampl\",\"grown\",\"salli\",\"word\",\"write\"]},\"corpusTokens\":[\"banana\",\"exampl\",\"grown\",\"salli\",\"word\",\"write\"],\"tokenStore\":{\"b\":{\"a\":{\"n\":{\"a\":{\"n\":{\"a\":{\"doc1\":2.7}}}}}},\"e\":{\"x\":{\"a\":{\"m\":{\"p\":{\"l\":{\"doc1\":2.5}}}}}},\"g\":{\"r\":{\"o\":{\"w\":{\"n\":{\"doc1\":0.2}}}}},\"s\":{\"a\":{\"l\":{\"l\":{\"i\":{\"doc1\":0.2}}}}},\"w\":{\"o\":{\"r\":{\"d\":{\"doc1\":0.2}}},\"r\":{\"i\":{\"t\":{\"e\":{\"doc1\":0.2}}}}}}}"
-
-
-exampleJsonIndex100somestring =
-    "{\"indexVersion\":\"1.0.0\",\"indexType\":\"_______some string\",\"documentStore\":{\"doc1\":[\"banana\",\"exampl\",\"grown\",\"salli\",\"word\",\"write\"]},\"corpusTokens\":[\"banana\",\"exampl\",\"grown\",\"salli\",\"word\",\"write\"],\"tokenStore\":{\"b\":{\"a\":{\"n\":{\"a\":{\"n\":{\"a\":{\"doc1\":2.7}}}}}},\"e\":{\"x\":{\"a\":{\"m\":{\"p\":{\"l\":{\"doc1\":2.5}}}}}},\"g\":{\"r\":{\"o\":{\"w\":{\"n\":{\"doc1\":0.2}}}}},\"s\":{\"a\":{\"l\":{\"l\":{\"i\":{\"doc1\":0.2}}}}},\"w\":{\"o\":{\"r\":{\"d\":{\"doc1\":0.2}}},\"r\":{\"i\":{\"t\":{\"e\":{\"doc1\":0.2}}}}}}}"
-
-
-exampleJsonIndex100default =
-    "{\"indexVersion\":\"1.0.0\",\"indexType\":\"- IndexTest Type -\",\"documentStore\":{\"doc1\":[\"banana\",\"exampl\",\"grown\",\"salli\",\"word\",\"write\"]},\"corpusTokens\":[\"banana\",\"exampl\",\"grown\",\"salli\",\"word\",\"write\"],\"tokenStore\":{\"b\":{\"a\":{\"n\":{\"a\":{\"n\":{\"a\":{\"doc1\":2.7}}}}}},\"e\":{\"x\":{\"a\":{\"m\":{\"p\":{\"l\":{\"doc1\":2.5}}}}}},\"g\":{\"r\":{\"o\":{\"w\":{\"n\":{\"doc1\":0.2}}}}},\"s\":{\"a\":{\"l\":{\"l\":{\"i\":{\"doc1\":0.2}}}}},\"w\":{\"o\":{\"r\":{\"d\":{\"doc1\":0.2}}},\"r\":{\"i\":{\"t\":{\"e\":{\"doc1\":0.2}}}}}}}"
-
-
-jsonIndexDefault =
-    "{\"indexVersion\":\"1.0.0\",\"indexType\":\"__IndexTest Type -\",\"documentStore\":{\"doc1\":[\"banana\",\"exampl\",\"grown\",\"salli\",\"word\",\"write\"]},\"corpusTokens\":[\"banana\",\"exampl\",\"grown\",\"salli\",\"word\",\"write\"],\"tokenStore\":{\"b\":{\"a\":{\"n\":{\"a\":{\"n\":{\"a\":{\"doc1\":2.7}}}}}},\"e\":{\"x\":{\"a\":{\"m\":{\"p\":{\"l\":{\"doc1\":2.5}}}}}},\"g\":{\"r\":{\"o\":{\"w\":{\"n\":{\"doc1\":0.2}}}}},\"s\":{\"a\":{\"l\":{\"l\":{\"i\":{\"doc1\":0.2}}}}},\"w\":{\"o\":{\"r\":{\"d\":{\"doc1\":0.2}}},\"r\":{\"i\":{\"t\":{\"e\":{\"doc1\":0.2}}}}}}}"
-
-
-loadIndexWithErr1 _ =
-    expectResultFailureMessage
-        "Fails to load an index with wrong index version."
-        "Error cannot load Index. Version supported is 1.1.0. Version tried to load is 1.0.1."
-    <|
-        Index.Load.loadIndexWith
-            [ config1 ]
-            exampleJsonIndex101
-
-
-loadIndexWithErr2 _ =
-    expectResultFailureMessage
-        "Fails to load an index with an indexType not in configuration provided."
-        "Error cannot load Index. Tried to load index of type \"__IndexTest Type -\". It is not in supported index configurations."
-    <|
-        Index.Load.loadIndexWith
-            [ config1 ]
-            exampleJsonIndex100
-
-
-loadIndexWith1 _ =
+loadIndexWith1Test : Test
+loadIndexWith1Test =
+    let
+        config =
+            { indexType = "not set"
+            , ref = .cid
+            , fields = [ ( .title, 5 ), ( .body, 1 ) ]
+            , listFields = []
+            , initialTransformFactories = []
+            , transformFactories = []
+            , filterFactories = []
+            }
+    in
     test "Load an index. really dumb check" <|
         \() ->
-            expectOk <|
-                mapDecodeErrorToString
-                    (Index.Load.loadIndexWith
-                        [ config2
-                        , config1
-                        ]
-                        exampleJsonIndex100somestring
-                    )
+            String.concat
+                [ "{\"indexVersion\":\"1.1.0\",\"indexType\":\"_______some string\","
+                , "\"documentStore\":{\"doc1\":[\"banana\",\"exampl\",\"grown\",\"salli\",\"word\",\"write\"]},"
+                , "\"corpusTokens\":[\"banana\",\"exampl\",\"grown\",\"salli\",\"word\",\"write\"],"
+                , "\"tokenStore\":{\"b\":{\"a\":{\"n\":{\"a\":{\"n\":{\"a\":{\"doc1\":2.7}}}}}},"
+                , "\"e\":{\"x\":{\"a\":{\"m\":{\"p\":{\"l\":{\"doc1\":2.5}}}}}},"
+                , "\"g\":{\"r\":{\"o\":{\"w\":{\"n\":{\"doc1\":0.2}}}}},"
+                , "\"s\":{\"a\":{\"l\":{\"l\":{\"i\":{\"doc1\":0.2}}}}},"
+                , "\"w\":{\"o\":{\"r\":{\"d\":{\"doc1\":0.2}}},"
+                , "\"r\":{\"i\":{\"t\":{\"e\":{\"doc1\":0.2}}}}}}}"
+                ]
+                |> Index.Load.loadIndexWith
+                    [ config
+                    , { config | indexType = "_______some string" }
+                    ]
+                |> TestUtils.expectOkWithGoodFailMessage
 
 
-indexfromString1 _ =
-    test "I can load index from string with ElmTextSearch.SimpleConfig." <|
+indexfromString1Test : Test
+indexfromString1Test =
+    test "Can succesfully load index from string with ElmTextSearch.SimpleConfig." <|
         \() ->
-            expectOk <|
-                mapDecodeErrorToString
-                    (ElmTextSearch.fromString
-                        { ref = .cid
-                        , fields =
-                            [ ( .title, 5 )
-                            , ( .body, 1 )
-                            ]
-                        , listFields = []
-                        }
-                        exampleJsonIndex100default
-                    )
+            String.concat
+                [ "{\"indexVersion\":\"1.1.0\",\"indexType\":\"-= ElmTextSearch Index Type 1 =-\","
+                , "\"documentStore\":{\"doc1\":[\"banana\",\"exampl\",\"grown\",\"salli\",\"word\",\"write\"]},"
+                , "\"corpusTokens\":[\"banana\",\"exampl\",\"grown\",\"salli\",\"word\",\"write\"],"
+                , "\"tokenStore\":{\"b\":{\"a\":{\"n\":{\"a\":{\"n\":{\"a\":{\"doc1\":2.7}}}}}},"
+                , "\"e\":{\"x\":{\"a\":{\"m\":{\"p\":{\"l\":{\"doc1\":2.5}}}}}},"
+                , "\"g\":{\"r\":{\"o\":{\"w\":{\"n\":{\"doc1\":0.2}}}}},"
+                , "\"s\":{\"a\":{\"l\":{\"l\":{\"i\":{\"doc1\":0.2}}}}},"
+                , "\"w\":{\"o\":{\"r\":{\"d\":{\"doc1\":0.2}}},"
+                , "\"r\":{\"i\":{\"t\":{\"e\":{\"doc1\":0.2}}}}}}}"
+                ]
+                |> ElmTextSearch.fromString
+                    { ref = .cid
+                    , fields =
+                        [ ( .title, 5 )
+                        , ( .body, 1 )
+                        ]
+                    , listFields = []
+                    }
+                |> TestUtils.expectOkWithGoodFailMessage
--- tests/TokenProcessorTests.elm
+++ tests/TokenProcessorTests.elm
@@ -1,54 +1,38 @@
-module TokenProcessorTests exposing (TokenArrayCase, TokenCase, tests, tokenCases, tokenizerTest, trimmerCases, trimmerTest)
+module TokenProcessorTests exposing (tokenizerTest, tokenizerTests, trimmerTest, trimmerTests)
 
 import Expect
 import Test exposing (..)
 import TokenProcessors
 
 
-type alias TokenCase =
-    ( String, String, List String )
+tokenizerTests : Test
+tokenizerTests =
+    describe "Lunr TokenProcessors tokenizer tests" <|
+        List.map tokenizerTest
+            [ ( "splitting simple strings into tokens"
+              , "this is a simple string"
+              , [ "this", "is", "a", "simple", "string" ]
+              )
+            , ( "downcasing tokens"
+              , "FOO BAR"
+              , [ "foo", "bar" ]
+              )
+            , ( "splitting strings with hyphens"
+              , "take the New York-San Francisco flight"
+              , [ "take", "the", "new", "york", "san", "francisco", "flight" ]
+              )
+            , ( "splitting strings with hyphens and spaces"
+              , "Solve for A - B"
+              , [ "solve", "for", "a", "b" ]
+              )
+            , ( "leading - in query should not cause extra token ?"
+              , "-misery! .appeal,"
+              , [ "misery!", ".appeal," ]
+              )
+            ]
 
 
-type alias TokenArrayCase =
-    ( String, List String, List String )
-
-
-tests : Test
-tests =
-    describe "Lunr TokenProcessors tests" <|
-        (List.map tokenizerTest tokenCases
-            ++ -- (List.map tokenizerArrayTest tokenArrayCases)
-               -- ++
-               List.map trimmerTest trimmerCases
-        )
-
-
-tokenCases : List TokenCase
-tokenCases =
-    [ ( "splitting simple strings into tokens"
-      , "this is a simple string"
-      , [ "this", "is", "a", "simple", "string" ]
-      )
-    , ( "downcasing tokens"
-      , "FOO BAR"
-      , [ "foo", "bar" ]
-      )
-    , ( "splitting strings with hyphens"
-      , "take the New York-San Francisco flight"
-      , [ "take", "the", "new", "york", "san", "francisco", "flight" ]
-      )
-    , ( "splitting strings with hyphens and spaces"
-      , "Solve for A - B"
-      , [ "solve", "for", "a", "b" ]
-      )
-    , ( "leading - in query should not cause extra token ?"
-      , "-misery! .appeal,"
-      , [ "misery!", ".appeal," ]
-      )
-    ]
-
-
-tokenizerTest : TokenCase -> Test
+tokenizerTest : ( String, String, List String ) -> Test
 tokenizerTest ( name, testString, expectedTokens ) =
     test name <|
         \() ->
@@ -57,33 +41,17 @@ tokenizerTest ( name, testString, expectedTokens ) =
                 (TokenProcessors.tokenizer testString)
 
 
-
--- tokenArrayCases : List TokenArrayCase
--- tokenArrayCases =
---     [ ( "downcasing token arrays"
---       , [ "Foo", "BAR" ]
---       , [ "foo", "bar" ]
---       )
---     ]
---
---
--- tokenizerArrayTest : TokenArrayCase -> Test
--- tokenizerArrayTest (name, testArray, expectedTokens) =
---     test name
---       <| assertEqual
---           expectedTokens
---           (TokenProcessors.tokenizerArray testArray)
-
-
-trimmerCases : List ( String, String )
-trimmerCases =
-    [ ( "023hello", "023hello" )
-    , ( "=hello", "hello" )
-    , ( "hello.", "hello" )
-    , ( ",hello,", "hello" )
-    , ( ",hello_,", "hello_" )
-    , ( "40%", "40" )
-    ]
+trimmerTests : Test
+trimmerTests =
+    describe "Lunr TokenProcessors trimmer tests" <|
+        List.map trimmerTest
+            [ ( "023hello", "023hello" )
+            , ( "=hello", "hello" )
+            , ( "hello.", "hello" )
+            , ( ",hello,", "hello" )
+            , ( ",hello_,", "hello_" )
+            , ( "40%", "40" )
+            ]
 
 
 trimmerTest : ( String, String ) -> Test
